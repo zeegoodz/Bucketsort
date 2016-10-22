10af82374cf5647b4e6cf55564002d1a90aba830
Zachary Goodspeed <zacharygoodspeed@u.boisestate.edu>
2016-10-22 09:43:39 -0600

    Finished Parallel Bucketsort

--- parallel-bucketsort.c
+++ parallel-bucketsort.c
@@ -6,7 +6,7 @@
 #include <prand.h>
 #include <mpi.h>
 
-/* 
+/*
  * Sequential bucketsort for randomly generated integers.
  *
  */
@@ -22,6 +22,7 @@ const int TRUE = 1;
 const int FALSE = 0;
 
 int *A;
+int *newA;
 int *recvBuff;
 int **parBucket; // bucket[i] = array holding the ith bucket
 int *parCapacity; // capacity[i] = capacity of ith bucket
@@ -29,6 +30,9 @@ int *parSize; // size[i] = next free location in ith bucket
 int **bucket;
 int *capacity;
 int *size;
+int myId;
+int count;
+int *allRecvBuff;
 
 
 void checkIfSorted(int *array, int n) {
@@ -38,6 +42,7 @@ void checkIfSorted(int *array, int n) {
 	sorted = TRUE;
 	for (i=0; i<n-1; i++)
 		if (array[i] > array[i+1]) {
+				printf("array[i] %d\narray[i+1] %d\n",array[i],array[i+1]);
 				sorted = FALSE;
 				break;
 		}
@@ -57,7 +62,7 @@ void checkIfSorted(int *array, int n) {
  */
 void generateInput(int *A, int fillN, long int seed, int myId, long long int offset) {
 	int i;
-    long long int startingPosition = 0;    
+    long long int startingPosition = 0;
 
 	srandom(seed);
     startingPosition = myId * offset;
@@ -97,8 +102,8 @@ void insertInBucket(int value, int bucketIndex) {
 		capacity[bucketIndex] = 2 * capacity[bucketIndex];
 		count_array_grows++;
 		if (DEBUG_LEVEL >= 1) {
-			fprintf(stderr, "Growing bucket %d from %d to %d elements\n",
-					bucketIndex, capacity[bucketIndex]/2, capacity[bucketIndex]);
+	//		fprintf(stderr, "Growing bucket %d from %d to %d elements\n",
+	//				bucketIndex, capacity[bucketIndex]/2, capacity[bucketIndex]);
 		}
 	}
 
@@ -118,13 +123,14 @@ void parInsertInBucket(int value, int bucketIndex) {
 		parCapacity[bucketIndex] = 2 * parCapacity[bucketIndex];
 		count_array_grows++;
 		if (DEBUG_LEVEL >= 1) {
-			fprintf(stderr, "Growing bucket %d from %d to %d elements\n",
-					bucketIndex, parCapacity[bucketIndex]/2, parCapacity[bucketIndex]);
+//			fprintf(stderr, "Growing bucket %d from %d to %d elements\n",
+//					bucketIndex, parCapacity[bucketIndex]/2, parCapacity[bucketIndex]);
 		}
 	}
 
 	parBucket[bucketIndex][parSize[bucketIndex]] = value;
 	parSize[bucketIndex]++;
+
 }
 /*
  * compareTo function for using qsort
@@ -147,11 +153,11 @@ void sortEachBucket(int numBuckets) {
 	}
 	if (DEBUG_LEVEL >= 2)
 		for (i=0; i<numBuckets; i++) {
-			fprintf(stderr, "bucket %d has %d elements\n", i, size[i]);
+	//		fprintf(stderr, "bucket %d has %d elements\n", i, size[i]);
 		}
 }
 
-/* 
+/*
  * Combine all buckets back into the original array to finish the sorting
  *
  */
@@ -162,9 +168,9 @@ void combineBuckets(int *A, int n, int numBuckets) {
 	for (i=0; i<numBuckets; i++) {
 		memcpy(A+start, bucket[i], sizeof(int)*size[i]);
 		start = start + size[i];
-		free(bucket[i]);
+	//	free(bucket[i]);
 	}
-	free(bucket);
+//	free(bucket);
 }
 
 void parCombineBuckets(int *A, int n, int numBuckets) {
@@ -174,9 +180,9 @@ void parCombineBuckets(int *A, int n, int numBuckets) {
 	for (i=0; i<numBuckets; i++) {
 		memcpy(A+start, parBucket[i], sizeof(int)*parSize[i]);
 		start = start + parSize[i];
-		free(parBucket[i]);
+	//	free(parBucket[i]);
 	}
-	free(parBucket);
+//	free(parBucket);
 }
 
 /*
@@ -187,7 +193,7 @@ void parCombineBuckets(int *A, int n, int numBuckets) {
  *
  */
 
-void sequentialBucketsort(int *recvBuff, int n, int numBuckets) {
+void sequentialBucketsort(int *A, int n, int numBuckets, int myid) {
 	int share;
 	int i;
 	int bucketRange;
@@ -206,6 +212,7 @@ void sequentialBucketsort(int *recvBuff, int n, int numBuckets) {
 		size[i] = 0;
 	}
 
+
 	bucketRange = RAND_MAX/numBuckets;
 	for (i=0; i<n; i++) {
 		bucketIndex = recvBuff[i]/bucketRange;
@@ -215,9 +222,9 @@ void sequentialBucketsort(int *recvBuff, int n, int numBuckets) {
 	}
 
 	sortEachBucket(numBuckets);
-	combineBuckets(recvBuff, n, numBuckets);
-	free(capacity);
-	free(size);
+	combineBuckets(A, n, numBuckets);
+//	free(capacity);
+//	free(size);
 }
 
 void parallelBucketsort(int *A, int myN, int numBuckets, int numProcs, int myId) {
@@ -227,7 +234,6 @@ void parallelBucketsort(int *A, int myN, int numBuckets, int numProcs, int myId)
 	int bucketIndex;
     int *sdispls;
     int *rdispls;
-    int *allRecvBuff;
 
 	share = myN / numBuckets;
 	share = share + (share * 11)/100; // 11% extra for overflow
@@ -243,12 +249,12 @@ void parallelBucketsort(int *A, int myN, int numBuckets, int numProcs, int myId)
 		parBucket[i] = (int *) malloc(sizeof(int)*share);
 		parCapacity[i] = share;
 		parSize[i] = 0;
+		allRecvBuff[i] = 0;
 	}
 
 	bucketRange = RAND_MAX/numBuckets;
 	for (i=0; i<myN; i++) {
 		bucketIndex = A[i]/bucketRange;
-
 		if (bucketIndex > numBuckets - 1) {
 				bucketIndex = numBuckets - 1;
         }
@@ -256,27 +262,32 @@ void parallelBucketsort(int *A, int myN, int numBuckets, int numProcs, int myId)
 	}
 
 	parCombineBuckets(A, myN, numBuckets);
-
     sdispls[0] = 0;
 
-    for (i=0;i<(numProcs-1);i++) {
-        sdispls[i+1] = sdispls[i] + parSize[i]; 
+    for (i=0;i<numBuckets;i++) {
+        sdispls[i+1] = sdispls[i] + parSize[i];
     }
 
-    MPI_Alltoall(parSize, 1, MPI_INT, allRecvBuff, 1, MPI_INT, MPI_COMM_WORLD);
+	int count = numBuckets/numProcs;
+	MPI_Alltoall(parSize, count, MPI_INT, allRecvBuff, count, MPI_INT, MPI_COMM_WORLD);
 
-    for (i=0;i<(numProcs-1);i++) {
-        rdispls[i+1] = rdispls[i] + allRecvBuff[i]; 
+
+	for(int i = 0; i<numBuckets; i++){
+		rdispls[i]=0;
+	}
+	for (int i=0;i<(numBuckets);i++) {
+        rdispls[i+1] = rdispls[i] + allRecvBuff[i];
     }
-    
-    MPI_Alltoallv(A, parSize, sdispls, MPI_INT, recvBuff, allRecvBuff, rdispls, MPI_INT,
-                  MPI_COMM_WORLD);
 
-    free(sdispls);
-    free(rdispls);
-    free(allRecvBuff);
-	free(parCapacity);
-	free(parSize);
+	if(myId == 1){
+
+	}
+
+	MPI_Alltoallv(A, parSize, sdispls, MPI_INT, recvBuff, allRecvBuff, rdispls, MPI_INT, MPI_COMM_WORLD);
+
+	for(int i = 0; i<numProcs; i++){
+		count += allRecvBuff[i];
+	}
 }
 
 void print_usage(char * program) {
@@ -284,23 +295,46 @@ void print_usage(char * program) {
            program);
 }
 
+void checkIfSortedParallel(int myId, int numProcs, int count, int *recvBuff){
 
+		int sendVal, recvVal;
+
+		sendVal = recvBuff[count-1];
+
+		if(myId == 0){
+			MPI_Send(&sendVal, 1, MPI_INT, 1, myId, MPI_COMM_WORLD);
+		}else if (myId == numProcs-1){
+			MPI_Recv(&recvVal, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}else{
+			MPI_Send(&sendVal, 1, MPI_INT, (myId+1), myId, MPI_COMM_WORLD);
+			MPI_Recv(&recvVal, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+
+		if(myId != 0){
+			if(recvVal<recvBuff[count-1]){
+				printf("All sorted values in process %d are leff than all sorted values of process %d.\n",myId-1, myId);
+			}else{
+				fprintf(stderr, "ERROR: Arrays are not sorted properly\n");
+			}
+
+		}
+}
 
 int main(int argc, char **argv) {
-	int n, myId, numProcs, myN;
+	int n, numProcs, myN;
 	int numBuckets;
     int myNumBuckets;
     int fillN;
     long long int offset;
 	unsigned int seed;
-	double startTime;
-	double totalTime;
+	double startTime=0;
+	double totalTime=0;
 
 	if (argc != 4) {
 		print_usage(argv[0]);
 		exit(1);
 	}
-       
+
 	numBuckets = atoi(argv[2]);
 	seed = atoi(argv[3]);
 	n = atoi(argv[1]);
@@ -309,47 +343,63 @@ int main(int argc, char **argv) {
 		print_usage(argv[0]);
 		exit(1);
 	}
- 
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myId);
     MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
 
 
     myN = n / numProcs;
-    myN += (n % numProcs);
+   if(myId == numProcs-1){
+		myN = myN + (n % numProcs);
+   }
     fillN = n / numProcs;
     offset = (long long int)fillN;
-    myNumBuckets = numProcs; 
+    myNumBuckets = numProcs;
 
 
+	newA = (int *) malloc(numProcs*sizeof(int) * n);
 	A = (int *) malloc(sizeof(int) * myN);
     recvBuff = (int *) malloc(sizeof(int) * myN);
-    
+
+	if(myId==0){
+	startTime = MPI_Wtime();
+	}
     if (myId == numProcs - 1) {
 	    generateInput(A, myN, seed, myId, offset);
     } else {
         generateInput(A, fillN,seed, myId, offset);
     }
 
- 	if (DEBUG_LEVEL >= 3) 
+ 	if (DEBUG_LEVEL >= 3)
 		printArray(A, myN);
 
-	startTime = MPI_Wtime();
 	parallelBucketsort(A, myN, myNumBuckets, numProcs, myId);
-    sequentialBucketsort(recvBuff, myN, myNumBuckets);
-	totalTime = MPI_Wtime() - startTime;
-	checkIfSorted(A, n);
+	int newCount=0;
+
+	for(int i=0;i<numProcs; i++){
+		newCount += allRecvBuff[i];
+	}
+	sequentialBucketsort(recvBuff, newCount, numBuckets, myId);
+	checkIfSorted(recvBuff, newCount);
+	checkIfSortedParallel(myId, numProcs, newCount, recvBuff);
+	checkIfSorted(newA, n);
 
 	if (DEBUG_LEVEL >= 1)
 		printf("Number of array grows is %d\n", count_array_grows);
- 	if (DEBUG_LEVEL >= 3) 
+ 	if (DEBUG_LEVEL >= 3)
  		printArray(A,n);
 
-	printf("bucketsort: n = %d  m = %d buckets seed = %d time = %lf seconds\n",
-		   n, numBuckets, seed,	totalTime/1000.0);
+	MPI_Barrier(MPI_COMM_WORLD);
+	if(myId==0){
 
-	free(A);
-    free(recvBuff);
+	totalTime = MPI_Wtime() - startTime;
+	printf("bucketsort: n = %d  m = %d buckets seed = %d time = =%.2f seconds\n",
+		   n, numBuckets, seed,	totalTime);
+	}
+//	free(A);
+ //   free(recvBuff);
+    MPI_Finalize();
 	exit(0);
 }
 
